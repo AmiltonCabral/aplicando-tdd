908b4e3cc4b2d1beb467236184b66716b98b24c6
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-03-02 20:38:34 -0300

    fatura paga se valor for suficiente ou a fatura ser gratis

--- ProcessadorDeBoletos/src/main/java/edu/ufcg/processadordeboletos/ProcessadorBoletos.java
+++ ProcessadorDeBoletos/src/main/java/edu/ufcg/processadordeboletos/ProcessadorBoletos.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class ProcessadorBoletos {
     public List<Pagamento> processarBoletos(List<Boleto> boletos, Fatura fatura) {
         List<Pagamento> pagamentos = new ArrayList<Pagamento>();
+        double totalPago = 0;
+
+        for (Boleto boleto : boletos) {
+            totalPago += boleto.getValorPago();
+        }
+
+        if (totalPago >= fatura.getValor()) {
+            fatura.setPaga();
+        }
+
         return pagamentos;
     }
 }
