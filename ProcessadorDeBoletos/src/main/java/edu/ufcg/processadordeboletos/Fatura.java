49b27a1a2f178e258c9b85ff88687a2a5780c5c6
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-03-02 20:19:01 -0300

    get e set valor fatura PAGA

--- ProcessadorDeBoletos/src/main/java/edu/ufcg/processadordeboletos/Fatura.java
+++ ProcessadorDeBoletos/src/main/java/edu/ufcg/processadordeboletos/Fatura.java
@@ -6,14 +6,24 @@ public class Fatura {
     private String nomeCliente;
     private double valorTotal;
     private LocalDate data;
+    private boolean isPaga;
 
     public Fatura(String nomeCliente, double valorTotal, LocalDate data) {
         this.nomeCliente = nomeCliente;
         this.valorTotal = valorTotal;
         this.data = data;
+        this.isPaga = false;
     }
 
     public double getValor() {
         return this.valorTotal;
+    }
+
+    public boolean isPaga() {
+        return this.isPaga;
+    }
+
+    public void setPaga() {
+        this.isPaga = true;
     }
 }
