6ef01d16ffd592149acca1d70f9680475f011ef6
welbber <welbervital01@gmail.com>
2024-03-03 12:21:31 -0300

    criando testes do service

---
+++ gerenciador/gerenciador-tarefas/README.md
@@ -0,0 +1,29 @@
+# Requisitos Funcionais e Não Funcionais da API de Gerenciamento de Tarefas
+
+## Requisitos Funcionais:
+
+1. **Cadastro de Tarefas:**
+    - Os usuários devem ser capazes de criar novas tarefas, inserindo informações como título, descrição, data de vencimento e prioridade.
+
+2. **Atualização de Tarefas:**
+    - Os usuários devem poder atualizar os detalhes de uma tarefa existente, incluindo a possibilidade de modificar o título, a descrição, a data de vencimento e a prioridade.
+
+3. **Exclusão de Tarefas:**
+    - Os usuários devem ser capazes de excluir tarefas da lista, caso a tarefa não seja mais relevante.
+
+4. **Listagem de Tarefas:**
+    - O sistema deve exibir uma lista de todas as tarefas do usuário, ordenadas por data de vencimento e prioridade.
+
+5. **Priorização de Tarefas:**
+    - Os usuários devem poder marcar tarefas como prioridades altas, médias ou baixas.
+
+## Requisitos Não Funcionais:
+
+1. **Usabilidade:**
+    - O sistema deve ser fácil de usar, proporcionando uma experiência intuitiva para os usuários na criação, atualização, exclusão e visualização de tarefas.
+
+2. **Eficiência:**
+    - O sistema deve ser eficiente na organização e apresentação da lista de tarefas, garantindo uma resposta rápida aos comandos dos usuários.
+
+3. **Segurança:**
+    - Deve ser implementado um mecanismo de segurança para proteger as informações das tarefas, garantindo que apenas usuários autorizados possam acessar, modificar ou excluir dados.
---
+++ gerenciador/gerenciador-tarefas/src/test/java/com/example/demo/service/TaskServiceTest.java
@@ -0,0 +1,96 @@
+package com.example.demo.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class TaskServiceTest {
+
+    @InjectMocks
+    private static TaskService taskService;
+
+    @Mock
+    private static TaskRepository taskRepository;
+
+    private static Task task;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        task = new Task("1", "Teste Titulo", "Teste descricao", Priority.MEDIUM, Status.DONE, LocalDate.now(), LocalDate.now());
+    }
+
+    @Test
+    public void saveSuccess() {
+
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+        Task taskTest = new Task("1", "Teste Titulo 1", "Teste descricao 1", Priority.HIGH, Status.DONE, LocalDate.now(), LocalDate.now());
+
+        Task taskTestReturn = taskService.save(taskTest);
+
+        assertEquals(taskTestReturn.getId(), "1");
+        assertEquals(taskTestReturn.getStatus(), taskTest.getStatus());
+        assertEquals(taskTestReturn.getTitle(), taskTest.getTitle());
+        assertEquals(taskTestReturn.getDescription(), taskTest.getDescription());
+        assertEquals(taskTestReturn.getDateConclusion(), taskTest.getDateConclusion());
+        assertEquals(taskTestReturn.getPriority(), taskTest.getPriority());
+    }
+
+    @Test
+    public void findAllSuccess() {
+
+        List<Task> tasks = List.of(task, new Task("2", "Teste Titulo 2", "Teste descricao 2", Priority.MEDIUM, Status.TO_DO, LocalDate.now(), LocalDate.now()));
+        when(taskRepository.findAll()).thenReturn(tasks);
+        assertEquals(taskService.findAll().get(0).getId(), tasks.get(0).getId());
+        assertEquals(taskService.findAll().get(1).getId(), tasks.get(1).getId());
+    }
+
+    @Test
+    public void findByIdSuccess() {
+
+        when(taskRepository.findById("1")).thenReturn(Optional.ofNullable(task));
+
+        assertEquals(taskService.findById("1").get().getId(), task.getId());
+    }
+
+    @Test
+    public void updateSuccess() {
+        when(taskRepository.save(task)).thenReturn(new Task("2", "Teste UPDATE", "Teste descricao UPDATE", Priority.LOW, Status.TO_DO, LocalDate.of(1,1,1), LocalDate.of(1,1,1)));
+
+        Task taskUpdate = taskService.update(task);
+
+
+        assertEquals(taskUpdate.getId(), "1");
+        assertEquals(taskUpdate.getTitle(), "Teste UPDATE");
+        assertEquals(taskUpdate.getDescription(), "Teste descricao UPDATE");
+        assertEquals(taskUpdate.getPriority(), Priority.LOW);
+        assertEquals(taskUpdate.getStatus(), Status.TO_DO);
+        assertEquals(taskUpdate.getDateStart(), LocalDate.of(1,1,1));
+        assertEquals(taskUpdate.getDateConclusion(), LocalDate.of(1,1,1));
+
+    }
+
+    @Test
+    public void deleteByIdSuccess() {
+
+        List<Task> tasks = List.of(task, new Task("2", "Teste Titulo 2", "Teste descricao 2", Priority.MEDIUM, Status.TO_DO, LocalDate.now(), LocalDate.now()));
+
+        taskService = mock(Taskservice.class);
+
+        verify(taskService, times(1)).deleteById("1");
+
+
+    }
+}
